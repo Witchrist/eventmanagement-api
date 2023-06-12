251f74a5e97a41c5744e7e2b8ad981815abdccf8
Christopher Alexander <christopherqueiroz@hotmail.com>
2023-06-12 16:10:19 -0300

    Criação de virtual projection para a classe venue

---
+++ src/main/java/com/eventmanagement/eventmanagementapi/entities/projections/StreetAdressProjection.java
@@ -0,0 +1,12 @@
+package com.eventmanagement.eventmanagementapi.entities.projections;
+
+import com.eventmanagement.eventmanagementapi.entities.Venue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "virtualProjection", types = {Venue.class})
+public interface StreetAdressProjection {
+
+    @Value("#{target.streetAdress} #{target.streetAdress2}")
+    String getCompleteVenueAdress();
+}
