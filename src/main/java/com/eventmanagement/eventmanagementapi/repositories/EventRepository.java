604b48fc13864aba3ecf170ef48d6936692a6d59
Christopher Alexander <christopherqueiroz@hotmail.com>
2023-06-12 16:19:30 -0300

    Inclusao de excerpts

--- src/main/java/com/eventmanagement/eventmanagementapi/repositories/EventRepository.java
+++ src/main/java/com/eventmanagement/eventmanagementapi/repositories/EventRepository.java
@@ -1,11 +1,14 @@
 package com.eventmanagement.eventmanagementapi.repositories;
 
 import com.eventmanagement.eventmanagementapi.entities.Event;
+import com.eventmanagement.eventmanagementapi.entities.projections.PartialEventProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(excerptProjection = PartialEventProjection.class)
 public interface EventRepository  extends JpaRepository<Event, Long> {
 
     Optional<Event> findById(@Param("resourceId") Long id);
