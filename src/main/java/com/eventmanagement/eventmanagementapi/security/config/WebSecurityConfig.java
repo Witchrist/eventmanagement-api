15d1ee09f8cf658a666bff8476820f1d4bdf87c3
Christopher Alexander <christopherqueiroz@hotmail.com>
2023-06-18 14:36:58 -0300

    Configurando autenticacao basica para a aplicacao

--- src/main/java/com/eventmanagement/eventmanagementapi/security/config/WebSecurityConfig.java
+++ src/main/java/com/eventmanagement/eventmanagementapi/security/config/WebSecurityConfig.java
@@ -2,11 +2,14 @@ package com.eventmanagement.eventmanagementapi.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
@@ -26,5 +29,16 @@ public class WebSecurityConfig {
     @Bean
     BCryptPasswordEncoder bCryptEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic().and().authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET,"/events/**").hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.POST,"/events").hasAnyRole( "ADMIN")
+                .requestMatchers(HttpMethod.PUT,"/events/**").hasAnyRole( "ADMIN")
+                .requestMatchers(HttpMethod.PATCH,"/events/**").hasAnyRole( "ADMIN");
+
+        return http.build();
     }
 }
